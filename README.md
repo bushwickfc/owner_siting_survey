38d75b37c2c10b68eee2a2ed48f6aaddd69aa1b7
Darren Klein <darren.a.klein@gmail.com>
2018-12-02 22:10:56 -0500

    Able to persist geometries and console log them as objects.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 > Map geometry collection tool
 
 A map interface owners can use to illustrate their preferred shopping areas.
+
+## Notes
+
+This app's JavaScript has been (mostly) linted to ESLint's Airbnb base standard.
--- app.js
+++ app.js
@@ -5,11 +5,41 @@ const drawControl = new L.Control.Draw({
     circle: false,
   },
   edit: {
-    featureGroup: featureGroup,
-  }
+    featureGroup,
+  },
 }).addTo(map);
+const submitButton = document.getElementById('submit');
+const postData = () => {
+  const geometryLayers = featureGroup._layers;
+  const geometryLayerKeys = Object.keys(geometryLayers);
+
+  if (!Object.keys(geometryLayerKeys).length) {
+    return false;
+  }
+
+  geometryLayerKeys.forEach((leafletId) => {
+    console.log(geometryLayers[leafletId]);
+  });
+};
+
+/////////////////////////////////
+// Map layer initialization
+/////////////////////////////////
 
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/skwidbreth.044joc73/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
   maxZoom: 18,
 }).addTo(map);
+
+////////////////////////////////
+// Draw tool behavior
+////////////////////////////////
+
+// Add the new geometry to the map.
+map.on('draw:created', e => featureGroup.addLayer(e.layer));
+
+////////////////////////////////
+// DOM events
+////////////////////////////////
+
+submitButton.addEventListener('click', postData);
