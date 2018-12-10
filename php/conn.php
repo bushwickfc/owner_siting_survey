398380f4330b1e6e5addd86d40ce8e94b2e46ad1
Darren Klein <darren.a.klein@gmail.com>
2018-12-09 20:03:48 -0500

    To test deploy, added listeners for env variables.

--- php/conn.php
+++ php/conn.php
@@ -1,5 +1,10 @@
 <?php
-include "../credentials.php";
+// include "../credentials.php";
+
+$host = $_ENV("HOST");
+$dbname = $_ENV("DBNAME");
+$user = $_ENV("USER");
+$password = $_ENV("PASSWORD");
 
 $conn = pg_connect("host={$host} dbname={$dbname} user={$user} password={$password}");
 ?>
