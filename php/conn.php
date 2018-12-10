52d5939dc97f54ab999a3ade7a0aa8a9e0ef564e
Darren Klein <darren.a.klein@gmail.com>
2018-12-09 20:15:03 -0500

    Get connection values from Heroku's DATABASE_URL env variable.

--- php/conn.php
+++ php/conn.php
@@ -1,10 +1,8 @@
 <?php
 // include "../credentials.php";
 
-$host = $_ENV("HOST");
-$dbname = $_ENV("DBNAME");
-$user = $_ENV("USER");
-$password = $_ENV("PASSWORD");
+// $conn = pg_connect("host={$host} dbname={$dbname} user={$user} password={$password}");
 
-$conn = pg_connect("host={$host} dbname={$dbname} user={$user} password={$password}");
+$conn = pg_connect(getenv("DATABASE_URL"));
+
 ?>
