75d750d0d26216ce3c95cc3cc89238c4a62f3612
Darren Klein <darren.a.klein@gmail.com>
2019-01-13 20:29:09 -0500

    change settings in results view.

--- scripts/results.js
+++ scripts/results.js
@@ -4,7 +4,7 @@ const map = L.map('map', {}).setView([40.705080, -73.928314], 15);
 // Map layer initialization
 /////////////////////////////////
 
-L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png', {
   maxZoom: 18,
   attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
@@ -16,8 +16,9 @@ const handleResponse = (response) => {
     features: [],
   };
   const featureStyle = {
-    color: 'blue',
+    color: 'black',
     stroke: 0,
+    fillOpacity: 0.03,
   };
 
   for (let i = 0; i < parsedResponse.length; i += 1) {
